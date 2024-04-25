aacd43602347780ddacba2a445254467b57707f2
Markus Ziermann <markus@fullmotion.de>
2024-04-25 09:53:20 +0200

    style: Rearrange dashboard items chore: github workflow

--- includes/display_lambda.cpp
+++ includes/display_lambda.cpp
@@ -4,61 +4,62 @@
         it.printf(400, 390, id(small), TextAlign::TOP_CENTER, translate["WAITING FOR DATA..."].c_str());
       } else
       {
-        // Forecast Boxes
-        it.rectangle(550,220,80,80);
-        it.rectangle(550,220,160,80);
-        it.rectangle(550,220,240,80);
-        it.rectangle(550,360,80,80);
-        it.rectangle(550,360,160,80);
-        it.rectangle(550,360,240,80);
 
         //Date and Time
-        it.printf(400, 10,     id(small),  TextAlign::CENTER_HORIZONTAL, germanTime.c_str());
-        //it.strftime(400, 30, id(big),    TextAlign::CENTER_HORIZONTAL, "%H:%M",         id(homeassistant_time).now());
-        it.printf(400,60,      id(medium), TextAlign::CENTER_HORIZONTAL, "%s",            getTimeInWords().c_str());
+        it.printf(400, 10,     id(small),  TextAlign::CENTER_HORIZONTAL, germanDate.c_str());
+        //it.strftime(400, 30, id(big),    TextAlign::CENTER_HORIZONTAL, "%H:%M", id(homeassistant_time).now());
+        it.printf(400,60,      id(medium), TextAlign::CENTER_HORIZONTAL, "%s", getTimeInWords().c_str());
 
         //Current Weather
-        it.printf(400, 160, id(tiny),           TextAlign::CENTER_HORIZONTAL, "%s: %s", translate["currently"].c_str(), translate[id(weather_now).state.c_str()].c_str());
-        it.printf(400, 180, id(font_mdi_large), TextAlign::CENTER_HORIZONTAL, "%s",     mdi_icon[id(weather_now).state.c_str()].c_str());
-        it.printf(400, 360, id(medium),         TextAlign::CENTER_HORIZONTAL, "%.1f°",  id(temp_now).state);
+        it.printf(400, 150, id(font_mdi_large), TextAlign::CENTER_HORIZONTAL, "%s",     mdi_icon[id(weather_now).state.c_str()].c_str());
+        it.printf(400, 330, id(medium),         TextAlign::CENTER_HORIZONTAL, "%.1f°",  id(temp_now).state);
+        it.printf(400, 410, id(tiny),           TextAlign::CENTER_HORIZONTAL, "%s",     translate[id(weather_now).state.c_str()].c_str());
 
         //Hourly Forecast
-        it.printf(670,160, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s:",   translate["forecast"].c_str());
-        it.printf(670,190, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s",    translate["hourly"].c_str());
-        it.printf(590,225, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h1).state.c_str()].c_str());
-        it.printf(670,225, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h2).state.c_str()].c_str());
-        it.printf(750,225, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h1).state.c_str()].c_str());
-        it.printf(590,265, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h1).state);
-        it.printf(670,265, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h2).state);
-        it.printf(750,265, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h3).state);
+        it.rectangle(545,215,80,80);
+        it.rectangle(545,215,160,80);
+        it.rectangle(545,215,240,80);
+        //it.printf(665,160, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s:",   translate["forecast"].c_str());
+        it.printf(665,175, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s",    translate["hourly"].c_str());
+        it.printf(585,220, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h1).state.c_str()].c_str());
+        it.printf(665,220, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h2).state.c_str()].c_str());
+        it.printf(745,220, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_h1).state.c_str()].c_str());
+        it.printf(585,260, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h1).state);
+        it.printf(665,260, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h2).state);
+        it.printf(745,260, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_h3).state);
 
         //Daily Forecast
-        it.printf(670,330, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s",    translate["daily"].c_str());
-        it.printf(590,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d1).state.c_str()].c_str());
-        it.printf(670,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d2).state.c_str()].c_str());
-        it.printf(750,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d1).state.c_str()].c_str());
-        it.printf(590,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d1).state);
-        it.printf(670,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d2).state);
-        it.printf(750,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d3).state);
+        it.rectangle(545,360,80,80);
+        it.rectangle(545,360,160,80);
+        it.rectangle(545,360,240,80);
+        it.printf(665,320, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%s",    translate["daily"].c_str());
+        it.printf(585,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d1).state.c_str()].c_str());
+        it.printf(665,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d2).state.c_str()].c_str());
+        it.printf(745,365, id(font_mdi_medium), TextAlign::CENTER_HORIZONTAL, "%s",    mdi_icon[id(weather_d1).state.c_str()].c_str());
+        it.printf(585,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d1).state);
+        it.printf(665,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d2).state);
+        it.printf(745,405, id(tiny),            TextAlign::CENTER_HORIZONTAL, "%.0f°", id(temp_d3).state);
 
         //Pool Temperature
-        it.printf(20,  160, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["pool-temperature"].c_str());
+        it.printf(20,  175, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["pool-temperature"].c_str());
 
         if (!isnan(id(pooltemperature).state)) {
-          it.printf(250, 160, id(small),           TextAlign::TOP_RIGHT, "%.2f °C", id(pooltemperature).state);
+          it.printf(250, 175, id(small),           TextAlign::TOP_RIGHT, "%.2f °C", id(pooltemperature).state);
         } else {
-          it.printf(250, 160, id(small),           TextAlign::TOP_RIGHT, "-");
+          it.printf(250, 175, id(small),           TextAlign::TOP_RIGHT, "-");
         }
 
         //Solar Data
-        it.printf(20,  210, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["home-lightning-bolt"].c_str());
-        it.printf(250, 210, id(small),           TextAlign::TOP_RIGHT, "%.2f W", id(power_comsumption).state);
-        it.printf(20,  260, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["solar-power-variant"].c_str());
+        it.printf(20,  220, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["home-lightning-bolt"].c_str());
+        it.printf(250, 220, id(small),           TextAlign::TOP_RIGHT, "%.0f W", id(power_comsumption).state);
+
+        it.printf(20,  265, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["solar-power-variant"].c_str());
         if (!isnan(id(solar_power).state)) {
-          it.printf(250, 260, id(small),           TextAlign::TOP_RIGHT, "%.2f W", id(solar_power).state);
+          it.printf(250, 265, id(small),           TextAlign::TOP_RIGHT, "%.0f W", id(solar_power).state);
         } else {
-          it.printf(250, 260, id(small),           TextAlign::TOP_RIGHT, "-");
+          it.printf(250, 265, id(small),           TextAlign::TOP_RIGHT, "-");
         }
+
         it.printf(20,  306, id(font_mdi_medium), TextAlign::TOP_LEFT,  mdi_icon["cash-multiple"].c_str());
         it.printf(250, 310, id(small),           TextAlign::TOP_RIGHT, "%.2f €", id(gesamtersparnis).state);
 
